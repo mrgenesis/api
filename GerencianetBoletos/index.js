13c8e8933aaddd13b8f5883863c8560e5608f275
Rosa, Genesis <mrgenesis@hotmail.com>
2020-07-30 17:01:41 -0400

    Definido arquivo principal para processar a criação dos boletos

---
+++ GerencianetBoletos/index.js
@@ -0,0 +1,36 @@
+const Gerencianet = require('gn-api-sdk-node');
+const dataVerify = require('./dataVerify');
+const configBankingBillet = require('./configBankingBillet');
+
+module.exports = async function (context, req) {
+  // if (recaptcha(req.body))
+  let { postInfo } = req.body;
+  if (dataVerify.all(postInfo.customer)) {
+    postInfo.product.value = dataVerify.value(postInfo.product.value);
+    let { options, paymentBody } = configBankingBillet(postInfo);
+    
+    gn = new Gerencianet(options);
+
+    await gn.oneStep({}, paymentBody)
+      .then((bankingBillet) => {
+        context.res = {
+          status: 201,
+          body: bankingBillet
+        }   
+      })
+      .catch((err) => {
+        context.res = {
+          status: 501,
+          body: { Error: 'Não foi possível criar seu boleto.', err }
+        }
+      });
+      return;
+  }
+
+  context.res = {
+    status: 501,
+    body: { Error: 'Não foi possível criar seu boleto.' }
+  }
+  
+
+}
